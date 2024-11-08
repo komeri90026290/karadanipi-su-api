cecaea83eada910ab3d71b6bc2a07e30b299f90f
komeri90026290 <kmm2359008@stu.o-hara.ac.jp>
2024-11-08 14:32:06 +0900

    login修正

--- index.js
+++ index.js
@@ -50,6 +50,23 @@ app.get('/test', async (req, res) => {
   }
 });
 
+
+app.post('/login', async (req, res) => {
+      const { username, password } = req.body;    
+      try {      const result = await pool.query(        
+        'SELECT * FROM test WHERE username = $1 AND password = $2',         
+        [username, password]      
+      );      
+      if (result.rows.length > 0) {        
+        return res.status(200).json({ message: 'ログイン成功', userId: result.rows[0].id });      
+      } else {        
+        return res.status(401).json({ error: 'ユーザー名またはパスワードが正しくありません' });      
+      }    
+    } catch (error) {      
+      console.error('Failed to login:', error);      
+      return res.status(500).json({ error: 'Failed to login' });    
+    }  
+  });
 // サーバーを起動
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
