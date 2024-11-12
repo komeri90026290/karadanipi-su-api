25344f0f7fdf0127531adcc6462ef5030ca479fc
komeri90026290 <kmm2359008@stu.o-hara.ac.jp>
2024-11-12 15:19:09 +0900

    useridに変更

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/login', async (req, res) => {
         [username, password]      
       );      
       if (result.rows.length > 0) {        
-        return res.status(200).json({ message: 'ログイン成功', userId: result.rows[0].id });      
+        return res.status(200).json({ message: 'ログイン成功', userId: result.rows[0].userid });      
       } else {        
         return res.status(401).json({ error: 'ユーザー名またはパスワードが正しくありません' });      
       }    
