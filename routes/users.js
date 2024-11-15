070288a93e9cede6201d4cc39d529ca6a9068180
komeri90026290 <kmm2359008@stu.o-hara.ac.jp>
2024-11-15 10:31:43 +0900

    mokuhyou修正

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+ 
 module.exports = (pool) => {
   router.get('/', async (req, res) => {
     try {
@@ -11,23 +11,24 @@ module.exports = (pool) => {
       return res.status(500).json({ error: 'Failed to fetch users' });
     }
   });
-
+ 
   router.put('/:id', async (req, res) => {
-    const { username, password, height, weight,userId } = req.body;
-
+    const { username, password, height, weight,userId,mokuhyou } = req.body;
+ 
     try {
       // 更新する項目を指定してSQLクエリを作成
       const result = await pool.query(
-        `UPDATE users SET 
-          username = COALESCE($1, username), 
+        `UPDATE users SET
+          username = COALESCE($1, username),
           password = COALESCE($2, password),
           height = COALESCE($3, height),
-          weight = COALESCE($4, weight) 
-         WHERE userid = $5 
+          weight = COALESCE($4, weight),
+          mokuhyou = COALESCE($5, mokuhyou)
+         WHERE userid = $6
          RETURNING *`,
-        [username, password, height, weight, userId]
+        [username, password, height, weight, userId,mokuhyou]
       );
-
+ 
       // 更新が成功したかチェック
       if (result.rows.length > 0) {
         return res.status(200).json(result.rows[0]);  // 更新後のユーザーデータを返す
@@ -39,7 +40,7 @@ module.exports = (pool) => {
       return res.status(500).json({ error: 'Failed to update user' });
     }
   });
-
+ 
   // ユーザーの追加
   router.post('/', async (req, res) => {
     const { username, password, height, weight } = req.body;
@@ -54,7 +55,7 @@ module.exports = (pool) => {
       return res.status(500).json({ error: 'Failed to add user' });
     }
   });
-
+ 
 // userId に基づいて特定のユーザーを取得
 router.get('/:id', async (req, res) => {
     const userId = req.params.id;
@@ -63,7 +64,7 @@ router.get('/:id', async (req, res) => {
             'SELECT * FROM users WHERE userid = $1',
             [userId]
         );
-
+ 
         if (result.rows.length > 0) {
             return res.status(200).json(result.rows[0]);  // 一致するユーザーを返す
         } else {
@@ -74,7 +75,7 @@ router.get('/:id', async (req, res) => {
         return res.status(500).json({ error: 'Failed to fetch user' });
     }
 });
-
-
+ 
+ 
   return router;
 };
